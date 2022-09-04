e98a85535bc01e46a9a1013d252f16bebdc04fd1
Niklas Solomon <niklassolomon@gmail.com>
2022-09-04 15:17:31 -0700

    Added all starting files and pseudo code

---
+++ server.js
@@ -0,0 +1,29 @@
+// View all departments
+// SELECT * FROM department
+
+// View all roles
+// SELECT * FROM role
+
+// View all employees
+// SELECT id, first_name, last_name FROM employee
+
+// Create new departments
+
+    // Prompt the user for the "name" of the department
+    // THEN run the query
+    // INSERT INTO department (name)
+    // VALUES ("Sales");
+
+        // THEN ask the user what they want to do next
+
+// Create a new role
+
+// Get the existing departments from the 'department' table
+
+    // THEN prompt the user for the "title", "salary", and "department" for the role
+
+        // THEN run the query
+        // INSERT INTO role (title, salary, department_id)
+        // VALUES ("Engineer", 120000, 1);
+
+            // THEN ask the user what they want to do next
